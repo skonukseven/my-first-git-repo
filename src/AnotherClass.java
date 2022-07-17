991e6a9908382f91b5f7b7266679637810a1be35
Ali <skonukseven@gmail.com>
2022-07-17 07:38:27 -0500

    fourth commit

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,5 +1,7 @@
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("This is another class");
+        System.out.println("I am doing changes");
+        System.out.println("One more change");
     }
 }
